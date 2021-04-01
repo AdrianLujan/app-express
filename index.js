e336aaa10c202524c08a2cce16f5b229dd757753
AdrianLujan <66387619+AdrianLujan@users.noreply.github.com>
2021-03-31 21:26:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ var express = require('express');
 var app = express();
 
 app.get('/',c_inicio);
-app.get('/saludo',c_saludo);
+app.get('/clientes',c_clientes);
+app.get('/productos',c_productos);
 
 function c_inicio(req,res){
     res.send('<h1>Express INICIO</h1>')
 }
-function c_saludo(req,res){
-    res.send('<h1>HOLA EXPRESS - ALUJAN </h1>')
-}
-function c_server(req,res){
-    console.log('port:7341');
+function c_clientes(req,res){
+    res.send('<h1>Cliente 1: Harold Arias</h1>')
+    res.send('<h2>Cliente 2: Ivan Oscco</h2>')
+    res.send('<h3>Cliente 3: Jafet Carrasco</h3>')
 }
 
-var server = app.listen(7341,c_server);
+function c_productos(req,res){
+    res.send('<h1>Producto 1: Monitor 24 pulgadas </h1>')
+    res.send('<h2>Producto 2: RAM 16Gb Corsair </h2>')
+    res.send('<h3>Producto 3: NVIDIA GTX 1650 </h3>')
+}
+
+function c_server(req,res){
+    console.log('port:5000');
+}
+
+var server = app.listen(5000,c_server);
